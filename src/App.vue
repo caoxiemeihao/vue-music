c8d906c61e3ffdad53be2f054ecade1941129117
草鞋没号 <308487730@qq.com>
2019-11-17 09:05:26 +0800

    添加stylus支持

--- src/App.vue
+++ src/App.vue
@@ -12,13 +12,9 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" scoped>
+@import "common/stylus/variable";
+
+#app
+  color: $color-theme
 </style>
