a1aed15724c1280394241c1c8d15825875df3a8f
草鞋没号 <308487730@qq.com>
2019-11-17 19:44:55 +0800

    singer 数据抓取

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,15 +1,38 @@
 <template>
-  <div>
-    歌手页面
+  <div class="singer">
+    
   </div>
 </template>
 
 <script>
+import { getSingerList } from '@api/singer'
+import { ERR_OK } from '@api/config'
+
 export default {
-  
+  data() {
+    return {
+      singers: []
+    }
+  },
+  created() {
+    this._getSingerList()
+  },
+  methods: {
+    async _getSingerList() {
+      const [err, res] = await getSingerList()
+      if (!err && res.code === ERR_OK) {
+        this.singers = res.data.list
+        console.log(this.singers)
+      }
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+.singer
+  position fixed
+  top 88px
+  bottom 0
+  width 100%
 </style>
