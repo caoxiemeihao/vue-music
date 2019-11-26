8f632447c553743ab197de7392b4adb3ea8ca978
草鞋没号 <308487730@qq.com>
2019-11-26 22:19:04 +0800

    歌手列表数据处理

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -7,6 +7,10 @@
 <script>
 import { getSingerList } from '@api/singer'
 import { ERR_OK } from '@api/config'
+import Singer from '@common/js/singer'
+
+const HOT_NAME = '热门'
+const HOT_SINGER_SIZE = 10
 
 export default {
   data() {
@@ -22,8 +26,49 @@ export default {
       const [err, res] = await getSingerList()
       if (!err && res.code === ERR_OK) {
         this.singers = res.data.list
-        console.log(this.singers)
+        console.log(this._normalizeSinger(this.singers))
       }
+    },
+    _normalizeSinger(list) {
+      const map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, idx) => {
+        if (idx < HOT_SINGER_SIZE) {
+          map.hot.items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name
+        }))
+      })
+      // map 排序
+      const hot = []
+      const ret = []
+      for (const [key, val] of Object.entries(map)) {
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return [...hot, ...ret]
     }
   }
 }
